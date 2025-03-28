d908e72ddab1b4a79a443098cd07c0265af3a02f
kristian0808 <kristianlleshaj@hotmail.com>
2025-03-28 13:56:03 +0100

    Fix notes module

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-import { NotesService } from './notes/notes.service';
-import { NotesController } from './notes/notes.controller';
-import { NotesService } from './notes.service';
-import { NotesController } from './notes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Mongoose } from 'mongoose';
+import { Note, NoteSchema } from './schemas/note.schema';
 
 @Module({
-  providers: [NotesService],
-  controllers: [NotesController]
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Note.name,
+                schema: NoteSchema,
+            },
+        ]),
+    ],
 })
 export class NotesModule {}
